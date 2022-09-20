103dc9771c0c660559ef25b7e6e104881d09932f
printingprinting <jessevarpness@icloud.com>
2022-09-19 23:48:22 -0500

    Add fist functions

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log("Hello World");
+
+
+//Randomly set a choice for the computer
+function getComputerChoice() {
+    let x = parseInt(Math.floor(Math.random() * 3) + 1);
+    let y = "";
+
+    if (x === 1) {
+        computerChoice = "Rock";
+    } else if (x === 2) {
+        computerChoice = "Paper";
+    } else if (x === 3) {
+        computerChoice = "Scissors";
+    }
+}
+
+let playerChoice = "";
+//Have the player enter a choice
+function getPlayerChoice() {
+    playerChoice = prompt("Choose rock, paper, or scissors")
+}
