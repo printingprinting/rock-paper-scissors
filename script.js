987c7066d07d3e4cde853cc9a0bd3827d59f6450
printingprinting <jessevarpness@icloud.com>
2022-09-20 20:56:20 -0500

    working console game

--- script.js
+++ script.js
@@ -6,16 +6,62 @@ function getComputerChoice() {
     let y = "";
 
     if (x === 1) {
-        computerChoice = "Rock";
+        return "rock";
     } else if (x === 2) {
-        computerChoice = "Paper";
+        return "paper";
     } else if (x === 3) {
-        computerChoice = "Scissors";
+        return "scissors";
     }
 }
 
-let playerChoice = "";
+
+
 //Have the player enter a choice
 function getPlayerChoice() {
-    playerChoice = prompt("Choose rock, paper, or scissors")
+    return prompt("Choose rock, paper, or scissors").toLowerCase();
+}
+
+
+//plays a round
+function playRound(computerSelection, playerChoice) {
+    if ((computerSelection === "paper") && (playerChoice === "rock")) {
+        return "computer wins " + computerWins++;
+    } else if ((computerSelection === "rock") && (playerChoice === "scissors")) {
+        return "computer wins " + computerWins++;
+    } else if ((computerSelection === "scissors") && (playerChoice === "paper")) {
+        return "computer wins " + computerWins++;
+    } else if ((computerSelection === "rock") && (playerChoice === "paper")) {
+        return "player wins " + playerWins++;
+    } else if ((computerSelection === "paper") && (playerChoice === "scissors")) {
+        return "player wins " + playerWins++;
+    } else if ((computerSelection === "scissors") && (playerChoice === "rock")) {
+        return "player wins " + playerWins++;
+    } else if (computerSelection === playerChoice) {
+        return "Your Tied!";
+    }
+}
+
+//initial values to track scores over the five rounds
+let computerWins = 0;
+let playerWins = 0;
+
+
+//call to play the game
+function game() {
+    //loops so that 5 rounds are played
+    for (let i = 0; i < 5; i++) {
+        let playerChoice = getPlayerChoice();
+        let computerSelection = getComputerChoice();
+        console.log(playRound(computerSelection, playerChoice));
+        // lets you know who won at the end of 5 rounds
+        if (i === 4) {
+            if (playerWins > computerWins) {
+                console.log("Player Wins!");
+            } else if (playerWins < computerWins) {
+                console.log("Computer Wins!");
+            } else {
+                console.log("You Tied!")
+            }
+        }
+    }
 }
